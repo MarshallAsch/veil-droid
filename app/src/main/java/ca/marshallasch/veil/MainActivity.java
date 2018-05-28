cf54f4fd4ffd6af46c5203cc5ae305fb15c44262
Marshall Asch <maasch@rogers.com>
2018-05-28 16:20:09 -0400

    Added Basic RightMesh stuff in the main activity

--- app/src/main/java/ca/marshallasch/veil/MainActivity.java
+++ app/src/main/java/ca/marshallasch/veil/MainActivity.java
@@ -1,13 +1,100 @@
 package ca.marshallasch.veil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashSet;
+
+import io.left.rightmesh.android.AndroidMeshManager;
+import io.left.rightmesh.android.MeshService;
+import io.left.rightmesh.id.MeshID;
+import io.left.rightmesh.mesh.MeshManager.PeerChangedEvent;
+import io.left.rightmesh.mesh.MeshManager.RightMeshEvent;
+import io.left.rightmesh.mesh.MeshStateListener;
+import io.left.rightmesh.util.RightMeshException;
+
+import static io.left.rightmesh.mesh.MeshManager.DATA_RECEIVED;
+import static io.left.rightmesh.mesh.MeshManager.PEER_CHANGED;
+import static io.left.rightmesh.mesh.MeshManager.REMOVED;
+
+public class MainActivity extends AppCompatActivity implements MeshStateListener
+{
+
+    private static final int DATA_PORT = 9182;
+   // private static final int DISCOVERY_PORT = 9183;       // This is not used yet, would be for
+                                                            // the local doc discovery network
+    // Set to keep track of peers connected to the mesh.
+    HashSet<MeshID> users = new HashSet<>();
+
+    // MeshManager instance - interface to the mesh network.
+    AndroidMeshManager meshManager = null;
+
+    private boolean meshActive = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called by the {@link MeshService} when the mesh state changes. Initializes mesh connection
+     * on first call.
+     *
+     * @param meshID our own user id on first detecting
+     * @param state state which indicates SUCCESS or an error code
+     */
+    @Override
+    public void meshStateChanged(MeshID meshID, int state)
+    {
+        switch(state) {
+            case SUCCESS: // Begin connecting
+
+                try {
+                    meshManager.bind(DATA_PORT);
+
+                    meshManager.on(DATA_RECEIVED, this::handleDataReceived);
+                    meshManager.on(PEER_CHANGED, this::handlePeerChanged);
+
+                } catch (RightMeshException e) {
+                    String status = "Error initializing the library" + e.toString();
+                    Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            case RESUME:  // over the mesh!
+
+                meshActive = true;
+                break;
+            case FAILURE:  // Mesh connection unavailable,
+            case DISABLED: // time for Plan B.
+                break;
+        }
+    }
+
+    /**
+     * Handles incoming data events from the mesh
+     *
+     * @param e event object from mesh
+     */
+    private void handleDataReceived(RightMeshEvent e) {
+        // TODO: 2018-05-28 Add in logic to handle the incoming data
+    }
+
+    /**
+     * Handles peer update events from the mesh - maintains a list of peers and updates the display.
+     *
+     * @param e event object from mesh
+     */
+    private void handlePeerChanged(RightMeshEvent e) {
+
+        // Update peer list.
+        PeerChangedEvent event = (PeerChangedEvent) e;
+        if (event.state != REMOVED && !users.contains(event.peerUuid)) {
+            users.add(event.peerUuid);
+        } else if (event.state == REMOVED){
+            users.remove(event.peerUuid);
+        }
     }
 }
