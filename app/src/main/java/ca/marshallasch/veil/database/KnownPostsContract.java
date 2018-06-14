850209cd5541106579a554dc87121b65e8735147
weihan <weihanli101@gmail.com>
2018-06-14 12:34:54 -0400

    added constructor + inline commenting + java doc header

---
+++ app/src/main/java/ca/marshallasch/veil/database/KnownPostsContract.java
@@ -0,0 +1,30 @@
+package ca.marshallasch.veil.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * This class holds the db query for creating the known posts table. The known posts
+ * table holds all the hashes of the known posts along with its associated comments.
+ * 
+ * @author Weihan
+ * @since 2018-06-14
+ * @version 1.0
+ */
+public final class KnownPostsContract {
+
+    // DB query string
+    static final String SQL_CREATE_KNOWN_HASHES = "CREATE TABLE " +  KnownPostsEntry.TABLE_NAME + "(" +
+            KnownPostsEntry._ID + " INTERGER PRIMARY KEY," +
+            KnownPostsEntry.COLUMN_COMMENT_HASH + "VARCHAR(36) UNIQUE," +
+            KnownPostsEntry.COLUMN_POST_HASH + "VARCHAR(36))";
+
+    // Constructor made private to eliminate people from accidentally instantiating this contract class
+    private KnownPostsContract(){}
+
+    // Inner class for defining the tables contents
+    public static final class KnownPostsEntry implements BaseColumns {
+        public static final String TABLE_NAME = "KnownPostsContract";
+        public static final String COLUMN_POST_HASH = "post_hash";
+        public static final String COLUMN_COMMENT_HASH = "comment_hash";
+    }
+}
