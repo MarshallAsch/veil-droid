0a914a858f8ef6870601a4a08b0610d102203fa5
Marshall Asch <maasch@rogers.com>
2018-07-03 11:13:21 -0700

    Added some more unit test to test the shared preferences, This should work using robolectric once the version gets updated to support android version 28. The new functions will currently fail.

--- app/src/test/java/ca/marshallasch/veil/utilities/UtilTest.java
+++ app/src/test/java/ca/marshallasch/veil/utilities/UtilTest.java
@@ -1,12 +1,20 @@
 package ca.marshallasch.veil.utilities;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import com.google.protobuf.Timestamp;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.Date;
 
+import ca.marshallasch.veil.MainActivity;
+import ca.marshallasch.veil.R;
 import ca.marshallasch.veil.proto.DhtProto;
 
 /**
@@ -14,6 +22,7 @@ import ca.marshallasch.veil.proto.DhtProto;
  * @version 1.0
  * @since 2018-06-13
  */
+@RunWith(RobolectricTestRunner.class)
 public class UtilTest
 {
     DhtProto.User author = DhtProto.User.newBuilder()
@@ -90,5 +99,87 @@ public class UtilTest
 
 
         // annotations say null can not be given for the message or the author
+    }
+
+
+    @Test
+    public void rememberUserName()
+    {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+
+        Util.rememberUserName(activity, "userName", "Password");
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+
+        String userName = sharedPref.getString(activity.getString(R.string.pref_username), null);
+        String password = sharedPref.getString(activity.getString(R.string.pref_passwords), null);
+
+        Assert.assertEquals("userName", userName);
+        Assert.assertEquals("Password", password);
+    }
+
+    @Test
+    public void getKnownUsername()
+    {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+
+        // make sure the shared preferences has a value
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(activity.getString(R.string.pref_username), "UserName1");
+        editor.apply();
+
+        String userName = Util.getKnownUsername(activity);
+
+        Assert.assertEquals("UserName1", userName);
+    }
+
+
+    @Test
+    public void getKnownPassword()
+    {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+
+        // make sure the shared preferences has a value
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(activity.getString(R.string.pref_passwords), "password1");
+        editor.apply();
+
+        String password = Util.getKnownPassword(activity);
+
+        Assert.assertEquals("password1", password);
+    }
+
+
+    @Test
+    public void clearKnownUser()
+    {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // make sure that the user is set
+        editor.putString(activity.getString(R.string.pref_username), "userName2");
+        editor.putString(activity.getString(R.string.pref_passwords), "password2");
+        editor.apply();
+
+        // clear the known user
+        Util.clearKnownUser(activity);
+
+        // check the values that are stored
+        String userName = sharedPref.getString(activity.getString(R.string.pref_username), null);
+        String password = sharedPref.getString(activity.getString(R.string.pref_passwords), null);
+
+        Assert.assertNull(userName);
+        Assert.assertNull(password);
     }
 }
