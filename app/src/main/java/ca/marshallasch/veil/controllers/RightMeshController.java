7f937e6193e410c15568b963083376c3744e96ab
weihan <weihanli101@gmail.com>
2018-07-10 00:13:04 -0400

    added rightmesh service to manifest + created basic rightmesh service skeleton + create rightmesh controller to hander rightmesh logic

---
+++ app/src/main/java/ca/marshallasch/veil/controllers/RightMeshController.java
@@ -0,0 +1,214 @@
+package ca.marshallasch.veil.controllers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import ca.marshallasch.veil.DataStore;
+import ca.marshallasch.veil.proto.Sync;
+import io.left.rightmesh.android.AndroidMeshManager;
+import io.left.rightmesh.android.MeshService;
+import io.left.rightmesh.id.MeshId;
+import io.left.rightmesh.mesh.MeshManager;
+import io.left.rightmesh.mesh.MeshStateListener;
+import io.left.rightmesh.util.RightMeshException;
+
+import static io.left.rightmesh.mesh.MeshManager.DATA_RECEIVED;
+import static io.left.rightmesh.mesh.MeshManager.PEER_CHANGED;
+import static io.left.rightmesh.mesh.MeshManager.REMOVED;
+
+/**
+ *
+ * All Rightmesh logic is abstracted into this class
+ *
+ * @author Weihan Li
+ * @version 1.0
+ * @created on 2018-07-09
+ * @name veil-droid
+ */
+
+public class RightMeshController implements MeshStateListener{
+    public static final String NEW_DATA_BROADCAST = "ca.marshallasch.veil.controllers.NEW_DATA_BROADCAST";
+    // MeshManager instance - interface to the mesh network.
+    AndroidMeshManager meshManager = null;
+    Context serviceContext = null;
+    public static final int DATA_PORT = 9182;
+
+    //MemoryStore instance - for storing data in local HashTable
+    DataStore dataStore = null;
+
+    private boolean meshActive = false;
+
+    /**
+     * Get a {@link AndroidMeshManager} instance and/or start RightMesh if it isn't already
+     * @param context service context to bind to
+     */
+    public void connect(Context context){
+        meshManager = AndroidMeshManager.getInstance(context, RightMeshController.this);
+        dataStore = DataStore.getInstance(context);
+        serviceContext = context;
+    }
+
+    /**
+     * Close the RightMesh connection and/or stopping the service is there is no other apps using
+     * RightMesh
+     */
+    public void disconnect(){
+        try{
+            if(meshManager != null){
+                meshManager.stop();
+            }
+        } catch (RightMeshException e){
+            Log.e("RightmeshContoller", "MeshManager Disconnect Failure.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handles incoming data events from the mesh
+     *
+     * @param e event object from mesh
+     */
+    private void handleDataReceived(MeshManager.RightMeshEvent e)
+    {
+        // TODO: 2018-05-28 Add in logic to handle the incoming data
+        final MeshManager.DataReceivedEvent event = (MeshManager.DataReceivedEvent) e;
+
+        Sync.Message message;
+        try {
+            message = Sync.Message.parseFrom(event.data);
+        }
+        catch (InvalidProtocolBufferException e1) {
+            e1.printStackTrace();
+            return;
+        }
+
+        Sync.SyncMessageType type = message.getType();
+
+        if (type == Sync.SyncMessageType.HASH_DATA) {
+
+            Log.d("DATA_RECEIVE", message.getData().toString());
+            dataStore.syncData(message.getData());
+        } else if (type == Sync.SyncMessageType.MAPPING_MESSAGE) {
+            Log.d("DATA_RECEIVE_MAP", message.getMapping().toString());
+
+            dataStore.syncDatabase(message.getMapping());
+
+            // notify anyone interested that the data store has been updated.
+            Intent intent = new Intent(NEW_DATA_BROADCAST);
+            LocalBroadcastManager.getInstance(serviceContext).sendBroadcast(intent);
+        } else if (type == Sync.SyncMessageType.REQUEST_DATA) {
+
+            Log.d("DATA_REQUEST", "recived request for data");
+            // if someone sent a message asking for data send a responce with everything
+
+            Sync.HashData hashData = dataStore.getDataStore();
+            Sync.MappingMessage mappingMessage = dataStore.getDatabase();
+
+            // send messages to the peer.
+            Sync.Message toSend = Sync.Message.newBuilder()
+                    .setType(Sync.SyncMessageType.HASH_DATA)
+                    .setData(hashData)
+                    .build();
+
+            try {
+                meshManager.sendDataReliable(event.peerUuid, DATA_PORT, toSend.toByteArray());
+
+                toSend = Sync.Message.newBuilder()
+                        .setType(Sync.SyncMessageType.MAPPING_MESSAGE)
+                        .setMapping(mappingMessage)
+                        .build();
+
+                meshManager.sendDataReliable(event.peerUuid, DATA_PORT, toSend.toByteArray());
+
+            }
+            catch (RightMeshException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Called by the {@link MeshService} when the mesh state changes. Initializes mesh connection
+     * on first call.
+     *
+     * @param meshId our own user id on first detecting
+     * @param state state which indicates SUCCESS or an error code
+     */
+    @Override
+    public void meshStateChanged(MeshId meshId, int state) {
+        switch(state) {
+            case SUCCESS: // Begin connecting
+
+                try {
+                    meshManager.bind(DATA_PORT);
+
+                    meshManager.on(DATA_RECEIVED, this::handleDataReceived);
+                    meshManager.on(PEER_CHANGED, this::handlePeerChanged);
+
+                } catch (RightMeshException e) {
+                    String status = "Error initializing the library" + e.toString();
+                    Toast.makeText(serviceContext, status, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.d("MESH", "initilized");
+
+            case RESUME:  // over the mesh!
+
+                meshActive = true;
+                break;
+            case FAILURE:  // Mesh connection unavailable,
+            case DISABLED: // time for Plan B.
+                break;
+        }
+    }
+
+    /**
+     * Handles peer update events from the mesh - maintains a list of peers and updates the display.
+     *
+     * @param e event object from mesh
+     */
+    private void handlePeerChanged(MeshManager.RightMeshEvent e) {
+
+        // Update peer list.
+        MeshManager.PeerChangedEvent event = (MeshManager.PeerChangedEvent) e;
+        if (event.state != REMOVED) {
+
+            if (event.peerUuid.equals(meshManager.getUuid())) {
+                Log.d("FOUND", "found loopback: " + event.peerUuid);
+                return;
+            }
+
+            Log.d("FOUND", "found user: " + event.peerUuid);
+
+
+
+            Sync.HashData hashData = dataStore.getDataStore();
+            Sync.MappingMessage mappingMessage =  dataStore.getDatabase();
+
+            // send messages to the peer.
+            try {
+
+                Sync.Message message = Sync.Message.newBuilder()
+                        .setType(Sync.SyncMessageType.HASH_DATA)
+                        .setData(hashData)
+                        .build();
+                meshManager.sendDataReliable(event.peerUuid, DATA_PORT, message.toByteArray());
+
+                message = Sync.Message.newBuilder()
+                        .setType(Sync.SyncMessageType.MAPPING_MESSAGE)
+                        .setMapping(mappingMessage)
+                        .build();
+                meshManager.sendDataReliable(event.peerUuid, DATA_PORT, message.toByteArray());
+            }
+            catch (RightMeshException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+}
--- app/src/main/java/ca/marshallasch/veil/services/VeilService.java
+++ app/src/main/java/ca/marshallasch/veil/services/VeilService.java
@@ -1,10 +1,67 @@
 package ca.marshallasch.veil.services;
 
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import ca.marshallasch.veil.controllers.RightMeshController;
+
 /**
  * @author Weihan Li
  * @version 1.0
  * @created on 2018-07-09
  * @name veil-droid
  */
-public class VeilService {
+public class VeilService extends IntentService {
+    public static final String START_FOREGROUND_ACTION = "ca.marshallasch.veil.action.startforeground";
+    public static final String STOP_FOREGROUND_ACTION = "ca.marshallasch.veil.action.stopforeground";
+    public static final int FOREGROUND_SERVICE_ID = 101;
+
+    private RightMeshController rightMeshController;
+    /**
+     * Default constructor that names the worker thread
+     */
+    public VeilService() {
+        super("VeilService");
+    }
+
+    /**
+     * Connects to RightMesh when service is started
+     */
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Intent stopForegroundIntent = new Intent(this, VeilService.class);
+        stopForegroundIntent.setAction(STOP_FOREGROUND_ACTION);
+        PendingIntent pendingIntent
+                = PendingIntent.getService(this, 0, stopForegroundIntent, 0);
+
+        //start RightMesh connection through controller using service context
+        rightMeshController = new RightMeshController();
+        rightMeshController.connect(this);
+    }
+
+    /**
+     * Disconnects from RightMesh when service is stopped
+     */
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        rightMeshController.disconnect();
+        rightMeshController = null;
+
+        //force kill service so it has no chance of recreating itself
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    /**
+     * IntentService calls this method from the default worker thread
+     * @param intent the intent that starts this service
+     */
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //TODO: add in work
+    }
 }
