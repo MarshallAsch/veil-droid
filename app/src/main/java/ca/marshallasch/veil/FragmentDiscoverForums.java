f7cb18067840937aff4649e16596772cdf513d7b
Marshall Asch <maasch@rogers.com>
2018-07-25 12:54:25 -0400

    Can now filter by tags.

--- app/src/main/java/ca/marshallasch/veil/FragmentDiscoverForums.java
+++ app/src/main/java/ca/marshallasch/veil/FragmentDiscoverForums.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -92,6 +93,22 @@ public class FragmentDiscoverForums extends Fragment implements SwipeRefreshLayo
         Spinner spinner = view.findViewById(R.id.filter);
         TagListAdapter tagListAdapter = new TagListAdapter(activity);
         spinner.setAdapter(tagListAdapter);
+
+        tagListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged()
+            {
+                List<String> tags = tagListAdapter.getSelectedTags();
+
+                StringBuilder filter = new StringBuilder();
+
+                for (String tag:tags) {
+                    filter.append(tag).append(":");
+                }
+
+                postListAdapter.getFilter().filter(filter.toString());
+            }
+        });
 
 
         refreshLayout.setOnRefreshListener(this);
